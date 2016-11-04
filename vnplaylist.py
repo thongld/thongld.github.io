99631c198094c2edbe2140490689acb4781574c4
thongld <thongld2012@gmail.com>
2016-11-03 21:57:01 -0700

    add GA

--- vnplaylist.py
+++ vnplaylist.py
@@ -1,20 +1,17 @@
-import httplib2, json, re, urllib
+#!/usr/bin/python
+#coding=utf-8
+import httplib2, json, re, urllib, os, uuid
 from kodiswift import Plugin, xbmc, xbmcgui, xbmcaddon
-plugin        = Plugin()
-http          = httplib2.Http(".cache")
-query_url     = "https://docs.google.com/spreadsheets/d/1zL6Kw4ZGoNcIuW9TAlHWZrNIJbDU5xHTtz-o8vpoJss/gviz/tq?gid={gid}&headers=1&tq={tq}"
-sheet_headers = {
+
+plugin         = Plugin()
+addon          = xbmcaddon.Addon("plugin.video.kodi4vn.vnplaylist")
+pluginrootpath = "plugin://plugin.video.kodi4vn.vnplaylist"
+http           = httplib2.Http(".cache", disable_ssl_certificate_validation=True)
+query_url      = "https://docs.google.com/spreadsheets/d/1zL6Kw4ZGoNcIuW9TAlHWZrNIJbDU5xHTtz-o8vpoJss/gviz/tq?gid={gid}&headers=1&tq={tq}"
+sheet_headers  = {
 	"User-Agent"      : "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.3; WOW64; Trident/7.0)",
 	"Accept-Encoding" : "gzip, deflate",
 }
-
-@plugin.route('/')
-def Home():
-	Section("0")
-
-@plugin.route('/section/<path>')
-def Section(path="0"):
-	return getItems("0")
 
 def getItems(url_path="0"):
 	query_url.format(
@@ -41,3 +38,44 @@ def getItems(url_path="0"):
 		items += [item]
 
 	return items
+
+@plugin.route('/')
+def Home():
+	GA()
+	Section("0")
+
+@plugin.route('/section/<path>')
+def Section(path="0"):
+	return plugin.finish(getItems("0"))
+
+def GA(title="Home",page="/"):
+	try:
+		ga_url    = "http://www.google-analytics.com/collect"
+		client_id = open(cid_path).read()
+		data      = {
+			'v'   : '1',
+			'tid' : 'UA-52209804-5',
+			'cid' : client_id,
+			't'   : 'pageview',
+			'dp'  : "VNPlaylist%s" % page,
+			'dt'  : "[VNPlaylist] - %s" % title
+		}
+		http.request(
+			ga_url, "POST",
+			body=urllib.urlencode(data)
+		)
+	except:
+		pass
+
+device_path = xbmc.translatePath('special://userdata')
+if os.path.exists(device_path)==False:
+	os.mkdir(device_path)
+cid_path = os.path.join(device_path, 'cid')
+search_history_path = os.path.join(device_path, 'search.p')
+
+if os.path.exists(cid_path)==False:
+	with open(cid_path,"w") as f:
+		f.write(str(uuid.uuid1()))
+
+if __name__ == '__main__':
+	plugin.run()
